a1c28844df81766d2834d2af6396138dcbefc016
juanangelaalma <juanlah77@gmail.com>
2022-09-14 13:09:00 +0700

    add index page

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,244 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:faker/faker.dart';
+import 'package:flutter_svg/svg.dart';
+import 'constants.dart';
+import 'dart:math' as math;
+
+import 'package:coolapp/models/movie.dart';
+import 'movie_card.dart';
 
 void main() {
-  runApp(LearnExtractWidget());
+  runApp(const MyApp());
 }
 
-class LearnExtractWidget extends StatelessWidget {
-  LearnExtractWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  var faker = new Faker();
-
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Extract Widget"),
+        appBar: buildAppBar(),
+        body: Body()
+      ),
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        padding: EdgeInsets.only(left: kDefaultPadding),
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+          icon: SvgPicture.asset("assets/icons/search.svg"),
+          onPressed: () {},
         ),
-        body: ListView.builder(
-          itemCount: 100,
-          itemBuilder: (context, index) {
-            return ChatItem(
-              imageUrl: "https://picsum.photos/id/${index}/200/200",
-              title: faker.person.name(),
-              subtitle: faker.lorem.sentence(),
-            );
-          }
-        )
+      ],
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          CategoryList(),
+          Genres(),
+          SizedBox(height: kDefaultPadding,),
+          MovieCarousel()
+        ],
+      )
+    );
+  }
+}
+
+class CategoryList extends StatefulWidget {
+  @override
+  _CategoryListState createState() => _CategoryListState();
+}
+
+class _CategoryListState extends State<CategoryList> {
+  int selectedCategory = 0;
+  List<String> categories = ["In Theater", "Box Office", "Coming Soon"];
+
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: kDefaultPadding / 2),
+      height: 60,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) => buildCategory(index, context),
+      ),
+    );
+  }
+
+  Padding buildCategory(int index, BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedCategory = index;
+          });
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              categories[index],
+              style: Theme.of(context).textTheme.headline5?.copyWith(
+                    fontWeight: FontWeight.w600,
+                    color: index == selectedCategory
+                        ? kTextColor
+                        : Colors.black.withOpacity(0.4),
+              ),
+            ),
+             Container(
+              margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+              height: 6,
+              width: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: index == selectedCategory
+                    ? kSecondaryColor
+                    : Colors.transparent,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
-class ChatItem extends StatelessWidget {
-  final String imageUrl;
-  final String title;
-  final String subtitle;
+class GenreCard extends StatelessWidget {
+  final String genre;
 
-  ChatItem({
-    required this.imageUrl,
-    required this.title,
-    required this.subtitle
-  });
+  const GenreCard({key, required this.genre}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(imageUrl),
+    return Container(
+      alignment: Alignment.center,
+      margin: EdgeInsets.only(left: kDefaultPadding),
+      padding: EdgeInsets.symmetric(
+        horizontal: kDefaultPadding,
+        vertical: kDefaultPadding / 4, // 5 padding top and bottom
       ),
-      title: Text(title),
-      subtitle: Text(
-        this.subtitle,
-        maxLines: 1,
-        overflow: TextOverflow.ellipsis,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black26),
+        borderRadius: BorderRadius.circular(20),
       ),
-      trailing: const Text("10:02 AM"),
+      child: Text(
+        genre,
+        style: TextStyle(color: kTextColor.withOpacity(0.8), fontSize: 16),
+      ),
     );
   }
+}
+
+class Genres extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    List<String> genres = [
+      "Action",
+      "Crime",
+      "Comedy",
+      "Drama",
+      "Horror",
+      "Animation"
+    ];
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+      height: 36,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: genres.length,
+        itemBuilder: (context, index) => GenreCard(genre: genres[index]),
+      ),
+    );
+  }
+}
+
+class MovieCarousel extends StatefulWidget {
+  @override
+  _MovieCarouselState createState() => _MovieCarouselState();
+}
+
+class _MovieCarouselState extends State<MovieCarousel> {
+  late PageController _pageController;
+  int initialPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      // so that we can have small portion shown on left and right side
+      viewportFraction: 0.8,
+      // by default our movie poster
+      initialPage: initialPage,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: AspectRatio(
+        aspectRatio: 0.85,
+        child: PageView.builder(
+          onPageChanged: (value) {
+            setState(() {
+              initialPage = value;
+            });
+          },
+
+          controller: _pageController,
+          physics: ClampingScrollPhysics(),
+          itemCount: movies.length,
+          itemBuilder: (context, index) => buildMovieSlider(index),
+        ),
+      ),
+    );
+  }
+
+  Widget buildMovieSlider(int index) => AnimatedBuilder(
+    animation: _pageController,
+    builder: (context, child) {
+      double value = 0;
+      if (_pageController.position.haveDimensions) {
+        value = index - double.parse(_pageController.page.toString());
+        value = (value * 0.038).clamp(-1, 1);
+      }
+      return AnimatedOpacity(
+        duration: Duration(milliseconds: 350),
+        opacity: initialPage == index ? 1 : 0.4,
+        child: Transform.rotate(
+          angle: math.pi * value,
+          child: MovieCard(movie: movies[index]),
+        ),
+      );
+    },
+  );
 }
---
+++ lib/movie_card.dart
@@ -0,0 +1,85 @@
+import 'package:animations/animations.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:coolapp/constants.dart';
+import 'package:coolapp/models/movie.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class MovieCard extends StatelessWidget {
+  final Movie movie;
+
+  const MovieCard({Key? key, required this.movie}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: OpenContainer(
+        closedElevation: 0,
+        openElevation: 0,
+        closedBuilder: (context, action) => buildMovieCard(context),
+        openBuilder: (context, action) {
+          return Scaffold(
+            backgroundColor: Colors.black,
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              leading: BackButton(
+                color: Colors.white,
+              ),
+            ),
+            body: Center(
+              child: Text(
+                movie.title,
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Column buildMovieCard(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              boxShadow: [kDefaultShadow],
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(movie.poster),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: kDefaultPadding / 2),
+          child: Text(
+            movie.title,
+            style: Theme.of(context)
+                .textTheme
+                .headline5
+                ?.copyWith(fontWeight: FontWeight.w600),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SvgPicture.asset(
+              "assets/icons/star_fill.svg",
+              height: 20,
+            ),
+            SizedBox(width: kDefaultPadding / 2),
+            Text(
+              "${movie.rating}",
+              style: Theme.of(context).textTheme.bodyText2,
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
